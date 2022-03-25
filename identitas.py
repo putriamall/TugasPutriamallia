a0ad5feb2019a0d5c2dfc4f17243e717007a302b
putri <putriamalliaramadhani@gmail.com>
2022-03-25 19:13:26 +0700

    tugas technical putri

---
+++ identitas.py
@@ -0,0 +1,12 @@
+nama     =  (input('Masukan Nama : '))
+nim      = (input('Masukan NIM : '))
+fakultas = (input('Masukan Fakultas : '))
+jurusan  = (input('Masukan Jurusan : '))
+asal     = (input('Masukan Asal Kota : '))
+alamat   = (input('Masukan Alamat : '))
+print ("Nama Saya"
+       ,nama,"NIM Saya"
+       ,nim,"Fakultas Saya Adalah"
+       ,fakultas,"Jurusan Saya adalah"
+       ,jurusan,"Saya berasal dari kota",asal,
+       "Saya tinggal di",alamat)
---
+++ temperatur.py
@@ -0,0 +1,18 @@
+#MEMBUAT PROGRAM KONVERSI TEMPERATUR
+print("\nPROGRAM PUTRI AMALLIA KONVERSI TEMPERATUR\n")
+
+#CELCIUS
+celcius = float(input('Masukan suhu dalam celcius :'))
+print("suhu adalah",celcius,"Celcius")
+
+#REAMUR
+reamur = (4/5)*celcius
+print("suhu dalam reamur adalah",reamur,"Reamur")
+
+#FARENHEIT
+farenheit = ((9/5)*celcius) +32
+print("suhu dalam farenheit adalah",farenheit,"Farenheit")
+
+#KELVIN
+kelvin = celcius + 273
+print("suhu dalam kelvin adalah",kelvin,"Kelvin")
